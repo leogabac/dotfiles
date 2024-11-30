86b7f52473c4719bafd7d210ea5b8e182882f7ce
leogabac <leogabac@gmail.com>
2024-11-30 14:38:37 -0600

    vpn tui

---
+++ tui/vpn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ===============================
+# A simple vpn selectio tui
+# ===============================
+
+# NOTE: This depends on gum https://github.com/charmbracelet/gum
+
+SELECTION=$(gum choose --header "Select VPN" "nordvpn" "zerotier")
+
+case "$SELECTION" in
+  "nordvpn")
+    sudo systemctl stop zerotier-one
+    sudo systemctl start nordvpnd.service
+    nordvpn connect
+    # to be able to access even if zerotier goes down
+    nordvpn set meshnet on
+  ;;
+  "zerotier")
+    sudo systemctl stop nordvpnd.service
+    sudo systemctl restart zerotier-one
+  ;;
+esac
+
+
