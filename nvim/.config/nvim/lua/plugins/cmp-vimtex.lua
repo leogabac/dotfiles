28de3aaa8bfac0b940d045e74b0bb24069250fcb
leogabac <leogabac@gmail.com>
2024-12-21 16:02:54 -0600

    now references work

--- nvim/.config/nvim/lua/plugins/cmp-vimtex.lua
+++ nvim/.config/nvim/lua/plugins/cmp-vimtex.lua
@@ -1,10 +1,10 @@
 return {
   -- add cmp-vimtex as a additional dependency
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "micangl/cmp-vimtex" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "vimtex" })
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = { "micangl/cmp-vimtex" },
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, { name = "vimtex" })
+  --   end,
+  -- },
 }
