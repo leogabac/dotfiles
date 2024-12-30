73cfd82358083ac4d800ba27f473a3414a709118
leogabac <leogabac@gmail.com>
2024-12-30 10:30:44 -0600

    added wezterm config

---
+++ wezterm/.config/wezterm/constants.lua
@@ -0,0 +1,6 @@
+local M = {}
+M.bg_image = os.getenv("HOME") .. "/dotfiles/graphics/.wallpapers/terminal.jpg"
+M.bg_image_darker = os.getenv("HOME") .. "/dotfiles/graphics/.wallpapers/terminal_darker.jpg"
+M.bg_image_darker_blurred = os.getenv("HOME") .. "/dotfiles/graphics/.wallpapers/terminal_darker_blurred.jpg"
+
+return M
---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,74 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local constants = require("constants")
+local commands = require("commands")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+
+-- ======================================
+-- FONT SETTINGS
+-- ======================================
+config.font_size = 15
+config.line_height = 1.0
+config.font = wezterm.font("0xProto Nerd Font Mono")
+
+-- ======================================
+-- COLORS
+-- ======================================
+
+config.colors = {
+    cursor_bg = 'white',
+    cursor_border = 'white',
+}
+
+-- ======================================
+-- WINDOW
+-- ======================================
+
+config.window_decorations ="RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+-- ======================================
+-- THEME
+-- ======================================
+
+config.window_background_image = constants.bg_image_darker_blurred
+config.color_scheme = 'Abernathy'
+
+-- ======================================
+-- MISCELLANEOUS
+-- ======================================
+
+config.enable_kitty_graphics=true
+config.max_fps = 60
+config.prefer_egl = true
+
+-- ======================================
+-- CUSTOM COMMANDS
+-- ======================================
+
+wezterm.on("augment-command-palette", function ()
+    return commands
+end)
+
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+-- and finally, return the configuration to wezterm
+return config
