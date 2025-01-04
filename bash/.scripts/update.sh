320c9083fcfb8a8d355dc9d5e37d1569fce770bf
leogabac <leogabac@gmail.com>
2025-01-04 00:07:09 -0600

    bug fix

--- bash/.scripts/update.sh
+++ bash/.scripts/update.sh
@@ -5,5 +5,5 @@ week_code() {
   echo "$(date +%y)w$(date +%U)"
 }
 
-sudo timeshift --create --comments "update $week_code"
+sudo timeshift --create --comments "update $(week_code)"
 sudo pacman -Syu
