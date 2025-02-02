b1210f46d2df059a6f06e6162f6cd99f90552172
leogabac <leogabac@gmail.com>
2025-02-01 23:33:45 -0600

    pdfmarkdown

--- leotools/bin/log.sh
+++ leotools/bin/log.sh
@@ -5,7 +5,7 @@
 # }
 #
 week_code() {
-  START_DAY="wednesday"
+  START_DAY="thursday"
   echo "$(date --date="last $START_DAY" +%y)w$(date --date="last $START_DAY" +%U)"
 }
 
@@ -16,7 +16,14 @@ cd ~/Dropbox/mnt/log/log25/ || exit
 # Create the file if it does not exist
 if [ ! -e "$FILENAME" ]; then
   touch $FILENAME
-  echo "#$(week_code)" > "$FILENAME"
+  echo "---" >> "$FILENAME"
+  echo "title: \"$(week_code)\"" >> "$FILENAME"
+  echo "author: \"leogabac\"" >> "$FILENAME"
+  echo "date: \"$(date)\"" >> "$FILENAME"
+  echo "toc: true" >> "$FILENAME"
+  echo "---" >> "$FILENAME"
+  echo "" >> "$FILENAME"
+  echo "#$(week_code)" >> "$FILENAME"
 
 fi
 
