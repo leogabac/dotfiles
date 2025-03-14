bbb401bb0cde7fb5ebe2a9cd8b48a29db962bec7
leogabac <leogabac@gmail.com>
2025-03-13 20:12:13 -0600

    allow to look within the passed dir as argument

--- bash/.scripts/cd-fzf.sh
+++ bash/.scripts/cd-fzf.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+    # if i pass an argument i want to search within that directory
+    current_dir=$1
+    selected=$(find $current_dir -mindepth 1 -maxdepth 3 -type d | fzf)
 else
     selected=$(find ~/ ~/Documents/ ~/Documents/projects ~/Dropbox/ ~/Dropbox/mnt/ -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
