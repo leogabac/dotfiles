5939a7eb23853ca3339fb75c5e2dda78b2629ca1
Leonardo Alanis <leogabac@gmail.com>
2025-04-03 11:34:14 -0600

    notebook server script

--- tmux/.scripts/tmux/notebook.sh
+++ tmux/.scripts/tmux/notebook.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-SESSION_NAME='thesis'
-WORK_DIR="~/Dropbox/mnt/thesis/"
+if [ $# -lt 1 ]; then
+    echo "Usage: ./tmux_run_command.sh <environment-alias>"
+    exit 1
+fi
+
+ENV_ALIAS="$1"
+POST_COMMAND="jupyter notebook --no-browser"
+
+SESSION_NAME="${ENV_ALIAS} notebook"
+WORK_DIR="$HOME"
 
 # Check if the tmux session already exists
 if tmux has-session -t "$SESSION_NAME" 2>/dev/null; then
@@ -11,6 +19,7 @@ else
     echo "Creating new session '$SESSION_NAME'..."
     # Create a new tmux session and run the commands
     tmux new-session -d -s "$SESSION_NAME"
-    tmux send-keys -t "$SESSION_NAME" "cd $WORK_DIR && clear && nvim main.tex" Enter
+    tmux send-keys -t "$SESSION_NAME" "cd $WORK_DIR && clear && $ENV_ALIAS && $POST_COMMAND" Enter
     tmux attach-session -t "$SESSION_NAME"
 fi
+i
