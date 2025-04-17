5c60e038e14dae1410d4c01719f1b46da5dec9ad
Leonardo Alanis <leogabac@gmail.com>
2025-04-16 19:53:13 -0600

    post install script

---
+++ leotools/.scripts/autoinstalls/post-install.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+echo -e "[\033[33mINFO\033[0m] Enabling bluetooth daemon"
+sudo systemctl enable bluetooth.service
+sudo systemctl start bluetooth.service
+
+relevant_packages=(
+    git
+    fastfetch
+    less
+    bluez
+    bluez-utils
+    bluez-deprecated-tools
+    pulseaudio-bluetooth
+    dosfstools
+    ntfsprogs
+    rsync
+    sshfs
+    )
+for package in ${relevant_packages[@]}; do
+    echo "sudo pacman -S --noconfirm ${package}"
+done
+
+
+echo -e "[\033[33mINFO\033[0m] Installing python build dependencies"
+python_build=(
+    base-devel
+    openssl
+    zlib
+    xz
+    tk
+    )
+for package in ${python_build[@]}; do
+    echo "sudo pacman -S --needed --noconfirm ${package}"
+done
+
+echo -e "[\033[33mINFO\033[0m] Installing command line utilities"
+cli=(
+    btop
+    nvtop
+    lsd
+    tmux
+    yazi
+    kitty
+    zoxide
+    fzf
+    timeshift
+    cronie
+    )
+for package in ${cli[@]}; do
+    echo "sudo pacman -S --noconfirm ${package}"
+done
+
+
+echo -e "[\033[33mINFO\033[0m] Installing dev tools"
+dev_tools=(
+    pyenv
+    python-virtualenv
+    jdk-openjdk
+    github-cli
+    marksman
+    )
+for package in ${dev_tools[@]}; do
+    echo "sudo pacman -S --noconfirm ${package}"
+done
+
+
+echo -e "[\033[33mINFO\033[0m] Installing desktop software"
+desktop_soft=(
+    inkscape
+    obs-studio
+    signal-desktop
+    noto-fonts-cjk
+    kcolorchooser
+    spectacle
+    filelight
+    flatpak
+    gnome-disk-utility
+    evince
+    )
+for package in ${desktop_soft[@]}; do
+    echo "sudo pacman -S --noconfirm ${package}"
+done
+
+echo -e "[\033[33mINFO\033[0m] Installing LaTeX dependencies"
+sudo pacman -S texlive zathura zathura-pdf-mupdf
+
+echo -e "[\033[33mINFO\033[0m] Installing yay"
+mkdir /tmp/yay
+git clone https://aur.archlinux.org/yay.git /tmp/yay
+cd /tmp/yay
+makepkg -si
+
+install proto nerdfont
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip
+cd ~/.local/share/fonts
+unzip 0xProto.zip
+rm 0xProto.zip
+fc-cache -fv
+
+zerotier
+sudo pacman -S zerotier-one
+sudo systemctl enable zerotier-one.service
+sudo systemctl start zerotier-one.service
+
+# some aur packages
+echo -e "[\033[33mINFO\033[0m] Installing AUR packages"
+aur_packages=(
+    brave-bin
+    onlyoffice-bin
+    zotero-bin
+    )
+for package in ${aur_packages[@]}; do
+    echo "yay -S --noconfirm ${package}"
+done
+
+
